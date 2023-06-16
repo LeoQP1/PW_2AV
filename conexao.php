f06160c8e52276710db66bdd2c70ead1c15e9d51
LeoPC <leoqueirozpontes@gmail.com>
2023-06-16 05:26:58 -0300

    Segundo commit

---
+++ conexao.php
@@ -0,0 +1,9 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "xammp";
+$dbName = "PW02";
+$port = 3306;
+
+// Cria a conexão com o banco de dados:
+$conn = mysqli_connect($servername, $username, $password, $dbName, $port);
