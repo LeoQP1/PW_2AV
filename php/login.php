2298461f09655a2b7a0b77fea4b26393ae77f8f3
LeoPC <leoqueirozpontes@gmail.com>
2023-06-17 19:22:29 -0300

    Problema no css

---
+++ php/login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Área do administrador</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <h1><img src="../img/logo_uepa.png" alt="Logo da UEPA"></h1>
+        </div>
+        <nav>
+            <ul class="menu">
+                <li><a href="../html/index.html" class="inicio" target="_self"><img src="../img/inicio.png" alt="Inicio"></a></li>
+                <li><a href="../html/http://ccnt.uepa.br/" target="_blank" rel="external">CCNT UEPA</a></li>
+                <li><a href="../html/curso.html" target="_self" rel="next">Estrutura do curso</a></li>
+                <li><a href="../html/avaliacao.html" target="_self" rel="next">Avaliação</a></li>
+                <li><a href="../html/corpo_docente.html" target="_self" rel="next">Corpo docente</a></li>
+                <li><a href="../html/infraestrutura.html" target="_self" rel="next">Estrutura do campus</a></li>
+                <li><a href="../html/colegiado.html" target="_self" rel="next">Colegiado</a></li>
+                <li><a href="../html/nucleo_docente.html" target="_self" rel="next">Núcleo docente</a></li>
+                <li><a href="../php/faleConosco.php" target="_self" rel="next">Fale conosco</a></li>
+                <li id="login"><a href="../php/login.php" target="_self" rel="next">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+
+    <footer>
+        <hr>
+        <div class="container">
+            <p>Universidade do Estado do Pará - UEPA</p>
+            <p>Rua do Una, nº 156 - Belém - Pará - Brasil - 66.050-540 - Telégrafo</p>
+            <p>Site Oficial: <a href="https://www.uepa.br" target="_blank" rel="external">www.uepa.br</a></p>
+            <p>Telefone: (91) 3299-2202</p>
+            <p>&copy; Copyright Diogo de Souza Corrêa, Léo Queiroz Pontes</p>
+
+            <ul class="social">
+                <li><a href="https://www.facebook.com/UepaOficial" target="_blank" rel="external"><img src="../img/facebook.png" alt="Facebook" type="image/png"></a></li>
+                <li><a href="https://www.instagram.com/uepaoficial/" target="_blank" rel="external"><img src="../img/instragram.png" alt="Instagram" type="image/png"></a></li>
+                <li><a href="https://twitter.com/Uepaoficial" target="_blank" rel="external"><img src="../img/twitter.png" alt="Twitter" type="image/png"></a></li>
+                <li><a href="https://www.youtube.com/@universidadedoestadodopara3606" target="_blank" rel="external"><img src="../img/youtube.png" alt="Youtube" type="image/png"></a></li>
+                <li><a href="https://www.flickr.com/photos/biuepa/" target="_blank" rel="external"><img src="../img/flickr.png" alt="Flickr" type="image/png"></a></li>
+            </ul>
+        </div>
+    </footer>
+</body>
+
+</html>
